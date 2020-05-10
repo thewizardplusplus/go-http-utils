9017e67d0b005dcaecc91202859a80ae00ecc062
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-05-10 23:00:00 +0300

    Add comments for the httputils.CatchingResponseWriter structure

--- catching_response_writer.go
+++ catching_response_writer.go
@@ -5,6 +5,17 @@ import (
 )
 
 // CatchingResponseWriter ...
+//
+// It wraps the http.ResponseWriter interface to save the last error
+// that occurred while calling the http.ResponseWriter.Write() method.
+//
+// Errors of writing via the http.ResponseWriter interface are important
+// to handle. See for details: https://stackoverflow.com/a/43976633
+//
+// Attention! The CatchingResponseWriter structure only supports methods
+// that are declared directly in the http.ResponseWriter interface. Any optional
+// interfaces (http.Flusher, http.Hijacker, etc.) aren't supported.
+//
 type CatchingResponseWriter struct {
 	http.ResponseWriter
 
@@ -12,6 +23,10 @@ type CatchingResponseWriter struct {
 }
 
 // NewCatchingResponseWriter ...
+//
+// It allocates and returns a new CatchingResponseWriter object wrapping
+// the provided http.ResponseWriter interface.
+//
 func NewCatchingResponseWriter(
 	writer http.ResponseWriter,
 ) *CatchingResponseWriter {
@@ -19,11 +34,14 @@ func NewCatchingResponseWriter(
 }
 
 // LastError ...
+//
+// It returns the last saved error from the Write() method of the wrapped
+// http.ResponseWriter interface.
+//
 func (writer CatchingResponseWriter) LastError() error {
 	return writer.lastError
 }
 
-// Write ...
 func (writer *CatchingResponseWriter) Write(p []byte) (n int, err error) {
 	n, err = writer.ResponseWriter.Write(p)
 	writer.lastError = err
