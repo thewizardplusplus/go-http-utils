e0e7525b4c3444ba342cf8dbc1e92449e9a897f9
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-05-13 23:00:00 +0300

    Add comments for the httputils.RunServer() function

--- run_server.go
+++ run_server.go
@@ -13,21 +13,47 @@ import (
 //go:generate mockery -name=Server -inpkg -case=underscore -testonly
 
 // Server ...
+//
+// It represents the interface of the http.Server structure used
+// by the RunServer() function.
+//
 type Server interface {
 	ListenAndServe() error
 	Shutdown(ctx context.Context) error
 }
 
 // RunServer ...
+//
+// It runs the provided Server interface via its ListenAndServe() method,
+// then waits for specified signals. If there're no signals specified,
+// the function will wait for any possible signals.
+//
+// After receiving the signal, the function will stop the server
+// via its Shutdown() method. The provided context will be used for calling
+// this method, and just for that.
+//
+// Attention! The function will return only after completing of ListenAndServe()
+// and Shutdown() methods of the server.
+//
+// Errors that occurred when calling ListenAndServe() and Shutdown() methods
+// of the server will be processed by the provided log.Logger interface. (Use
+// the github.com/go-log/log package to wrap the standard logger.) The fact
+// that an error occurred will be reflected in the boolean flag returned
+// by the function.
+//
 func RunServer(
 	shutdownCtx context.Context,
 	server Server,
 	logger log.Logger,
 	interruptSignals ...os.Signal,
 ) (ok bool) {
+	// it's used to wait for the shutdown goroutine to complete
 	var waiter sync.WaitGroup
 	waiter.Add(1)
 
+	// it's used to complete the shutdown goroutine if this function ends earlier
+	// (before receiving any signal); it'll happen when an error occurs
+	// when calling the ListenAndServe() method of the provided server
 	interruptCtx, interruptCtxCancel := context.WithCancel(context.Background())
 	defer interruptCtxCancel()
 
