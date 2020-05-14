53b69abd2f0c8ea0dd69f02b801a05651eb4e343
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-05-14 23:00:00 +0300

    Add the comment for the httputils.SPAFallbackMiddleware() function

--- spa_fallback_middleware.go
+++ spa_fallback_middleware.go
@@ -8,6 +8,22 @@ import (
 )
 
 // SPAFallbackMiddleware ...
+//
+// A SPA often manages its routing itself, so all relevant requests must
+// be handled by an index.html file. A backend, in turn, must be responsible
+// for an API and distribution of static files.
+//
+// To separate these types of requests, the following heuristic is used.
+// If the request uses the GET method and contains the text/html value
+// in the Accept header, the index.html file is returned (more precisely, a path
+// part of a request URL is replaced to "/"). All other requests are processed
+// as usual. The fact is that any routing requests sent from a modern browser
+// meet described requirements.
+//
+// This solution is based on the proxy implementation in the development server
+// of the Create React App project. See:
+// https://create-react-app.dev/docs/proxying-api-requests-in-development/
+//
 func SPAFallbackMiddleware() mux.MiddlewareFunc {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(
